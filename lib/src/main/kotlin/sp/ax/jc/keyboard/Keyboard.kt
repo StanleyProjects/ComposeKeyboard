0935cfa55382047b8fbc83d3a573dd1567e2d5f9
Stanley Wintergreen <kepocnhh@gmail.com>
2024-03-07 18:19:01 +0300

    todo

--- lib/src/main/kotlin/sp/ax/jc/keyboard/Keyboard.kt
+++ lib/src/main/kotlin/sp/ax/jc/keyboard/Keyboard.kt
@@ -127,6 +127,8 @@ fun Keyboard(
     enabled: Boolean = true,
     onClick: (Char) -> Unit,
 ) {
+    // todo new line
+    // todo backspace
     val rows = listOf(
         charArrayOf('1', '2', '3', '4', '5', '6', '7', '8', '9', '0'),
         charArrayOf('q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'),
