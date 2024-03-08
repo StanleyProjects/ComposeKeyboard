ad181af807516dea1a9f01cbcb2977f5eef28c64
Stanley Wintergreen <kepocnhh@gmail.com>
2024-03-08 20:22:13 +0300

    fix code style

--- lib/src/main/kotlin/sp/ax/jc/keyboard/Keyboard.kt
+++ lib/src/main/kotlin/sp/ax/jc/keyboard/Keyboard.kt
@@ -2,7 +2,6 @@ package sp.ax.jc.keyboard
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.Indication
-import androidx.compose.foundation.background
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -19,7 +18,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
